b777e519b78b258f7765bec3ee432ce073863f6f
Aime145 <152657690+Aime145@users.noreply.github.com>
2025-12-25 01:44:03 +0100

    Ajout du fichier Python

--- Signature_EFM.py
+++ Signature_EFM.py
@@ -1,5 +1,24 @@
-A=1+1
-B=2+2**A-10
-C=A+B
-print(C)
+import numpy as np
+import signature.path_signature as ps
+
+# Définition du chemin
+path = np.array([[0, 0], [1, 2], [2, 1]])
+
+trunc = 2
+lam = 0.5
+
+# Grille de temps
+t_grid = np.array([0, 1, 2], dtype=float)
+
+# Signature classique
+sig = ps.path_to_signature(path, trunc)
+
+# Signature à mémoire finie : attention à l’ordre !
+sig_fm = ps.path_to_fm_signature(path, trunc, t_grid, lam)
+
+print("Signature classique :")
+print(sig)
+
+print("\nSignature à mémoire finie :")
+print(sig_fm)
 
